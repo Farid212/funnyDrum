86b8938ed76edd54d06ebb8b016032df4d4b061d
Farid212 <farid.hammouten@gmail.com>
2018-04-09 07:07:17 +0200

    little changement

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,19 @@
 window.addEventListener('keydown', function(e){
     //console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
-    console.log(audio);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
     audio.play();
+    console.log(key);
+    key.classList.toggle('playing');
+
+    function removeTransition(e){
+        if(e.propertyName !== 'tranform') return;
+        console.log(e.propertyName);
+    }
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionned', removeTransition));
+    
 });
